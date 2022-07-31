ed5cda699194c0ad50c06f8d80d6631996591a4b
whoisgalih <galihakbar.ga91@gmail.com>
2022-07-31 15:18:28 +0700

    Initial Commit

---
+++ FoodFinder/FoodFinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodFinderApp.swift
+//  FoodFinder
+//
+//  Created by Galih Akbar on 31/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FoodFinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
